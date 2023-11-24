d5174a48e9cdb06a213b66c568a4d2f292f497aa
cyril <telliercyril@gmail.com>
2023-11-24 15:23:28 +0100

    navbar link de Commentaires

---
+++ src/Controller/CommentairesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentairesController extends AbstractController
+{
+    #[Route('/commentaires', name: 'app_commentaires')]
+    public function index(): Response
+    {
+        return $this->render('commentaires/index.html.twig', [
+            'controller_name' => 'CommentairesController',
+        ]);
+    }
+}
