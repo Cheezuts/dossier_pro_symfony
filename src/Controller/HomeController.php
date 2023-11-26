3f53713cebcc443815087f4ff4c6fac53f0e7c70
cyril <telliercyril@gmail.com>
2023-11-26 12:24:26 +0100

    affichage article dans vu ( à finir pour img )

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $article = $articleRepository->findAll();
         return $this->render('home/index.html.twig', [
+            'article' => $article,
             'controller_name' => 'HomeController',
         ]);
     }
